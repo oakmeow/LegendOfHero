382e10472a348d98bfacd02a123bc5a4bf254118
oakmeow <oakmeow@gmail.com>
2024-01-24 04:00:04 +0700

    :hammer: PlayerHealt script Initial

---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int startHealth = 100;
+    [SerializeField] float timeLastHit = 2f;
+    private float timer = 0f;
+    private CharacterController characterController;
+    private Animator anim;
+    [SerializeField] private int currentHealth;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        currentHealth = startHealth;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!GameManager.instance.GameOver && timer >= timeLastHit)
+        {
+            if (other.tag == "weapon")
+            {
+                takeHit();
+                timer = 0;
+            }
+        }
+    }
+
+    private void takeHit()
+    {
+        if (currentHealth > 0)
+        {
+            anim.Play("Hurt");
+            currentHealth -= 10;
+            GameManager.instance.PlayerHit(currentHealth);
+        }
+        if (currentHealth <= 0)
+        {
+            KillPlayer();
+        }
+    }
+
+    private void KillPlayer()
+    {
+        GameManager.instance.PlayerHit(currentHealth);
+        anim.SetTrigger("HeroDie");
+        characterController.enabled = false;
+    }
+}
