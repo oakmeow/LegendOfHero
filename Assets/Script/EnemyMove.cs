723bdffa1c5c44d84f5563565d91b26e68fb0b18
oakmeow <oakmeow@gmail.com>
2024-01-31 22:27:03 +0700

    Update EnemyMove.cs

--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -8,19 +8,26 @@ public class EnemyMove : MonoBehaviour
     public Transform target;
     private NavMeshAgent nav;
     private Animator anim;
+    private EnemyHealth enemyHealth;
 
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        target = GameManager.instance.Player.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!GameManager.instance.GameOver)
+        if (!GameManager.instance.GameOver && enemyHealth.IsAlive)
         {
             nav.SetDestination(target.position);
+        }
+        else if (!GameManager.instance.GameOver || GameManager.instance.GameOver && !enemyHealth.IsAlive)
+        {
+            nav.enabled = false;
         }
         else
         {
