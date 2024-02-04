2ea2ed20736bb3c77766dc28fa63b03cb0c760dd
oakmeow <oakmeow@gmail.com>
2024-02-05 02:07:17 +0700

    Create AnimationEvents.cs
    +Fix animator event Error

---
+++ Assets/Script/AnimationEvents.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+    public void FireArrow()
+    {
+
+    }
+
+    public void EnemyBeginAttack()
+    {
+
+    }
+
+    public void EnemyEndAttack()
+    {
+
+    }
+
+    public void BeginAttack()
+    {
+        
+    }
+
+    public void EndAttack()
+    {
+        
+    }
+}
